2ba3becc3caa527be7e455107f1f98c9ec37ac26
gidsi <gidsi@chaospott.de>
2020-07-24 16:03:02 +0200

    Adds xss-lock, configure suspend

--- test.sh
+++ test.sh
@@ -3,11 +3,15 @@
 set -e
 set -x
 
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
 for role in $(ls roles)
 do
         cd roles/$role
         if [[ -d ./molecule ]]; then
-            molecule test
+            python -m molecule test
         fi
   cd ../..
 done
