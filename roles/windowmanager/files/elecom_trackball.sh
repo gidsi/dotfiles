db50859a6d6caa8832a8f85aa50c5bce04945b77
timm <szigat+nospam@sipgate.de>
2022-01-14 12:37:21 +0100

    Add kvm switch / neo fixes

--- roles/windowmanager/files/elecom_trackball.sh
+++ roles/windowmanager/files/elecom_trackball.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-TRACKBALL_ID=$(xinput --list --id-only "pointer:ELECOM TrackBall Mouse DEFT Pro TrackBall Mouse")
+POINTER="HID 056e:0131 Mouse"
+#POINTER="ELECOM TrackBall Mouse DEFT Pro TrackBall Mouse"
+
+TRACKBALL_ID=$(xinput --list --id-only "$POINTER")
 xinput set-prop "$TRACKBALL_ID" 'libinput Button Scrolling Button' 11
 xinput set-prop "$TRACKBALL_ID" 'libinput Scroll Method Enabled' 0 0 1
 xinput set-button-map "$TRACKBALL_ID" 1 10 3 4 5 6 7 8 2 2 11 12 13 14 15 16
